3c6d230bd3283a846dd6c17c65b9e8443be60494
alanaynbund <aynbunda@gmail.com>
2023-02-15 21:05:26 -0500

    done

--- src/views/Person.jsx
+++ src/views/Person.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios"
+import { useParams } from 'react-router-dom'
 
 const Person = () => {
+    const {id} = useParams()
+    const [person, setPerson] = useState()
+
+
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/people/${id}`)
+            .then(res => {
+                setPerson(res.data)
+            })
+            .catch(err=>console.log(err))
+    }, [id])
+
+
+
     return (
-        <div>person</div>
+        person?
+        <div>
+            <h1>{person.name}</h1>
+            <p>Height: {person.height}</p>
+            <p>Hair Color: {person.hair_color}</p>
+            <p>Skin Color: {person.skin_color}</p>
+            <p>Birth Year: {person.birth_year}</p>
+        </div>:
+        <h1>Loading...</h1>
     )
 }
 
--- src/views/Planet.jsx
+++ src/views/Planet.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios"
+import { useParams } from 'react-router-dom'
 
 const Planet = () => {
+    const {id} = useParams()
+    const [planet, setPlanet] = useState()
+
+
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/planets/${id}`)
+            .then(res => {
+                setPlanet(res.data)
+            })
+            .catch(err=>console.log(err))
+    }, [id])
+
+
+
     return (
-        <div>person</div>
+        planet?
+        <div>
+            <h1>{planet.name}</h1>
+            <p>Diameter: {planet.diameter}</p>
+            <p>Gravity: {planet.gravity}</p>
+            <p>Terrain: {planet.terrain}</p>
+            <p>Rotation Period: {planet.rotation_period}</p>
+        </div>:
+        <h1>Loading...</h1>
     )
 }
 
